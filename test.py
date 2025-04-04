4aff852e121a6ec1883cf221233f44a494aa8b22
qianyanglm <qianyanglm@gmail.com>
2025-04-05 00:50:44 +0800

    Changes

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('hello')
+import sys
+
+# 输出Python版本
+print(f"Python version: {sys.version}")
+print(f"Python version info: {sys.version_info}")
+print(f"Python version info: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")
+#输出hello world 力
+print("Hello World! 力")
