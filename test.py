46e8fe40cfe5b50c6b8d149d508dbf8782e7c70e
qianyanglm <qianyanglm@gmail.com>
2025-04-05 00:53:46 +0800

    Changes

--- test.py
+++ test.py
@@ -6,3 +6,4 @@ print(f"Python version info: {sys.version_info}")
 print(f"Python version info: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")
 #输出hello world 力
 print("Hello World! 力")
+print(1+1)
